04a683c7b14930c90897a5e855eb32e1032e787b
marwen <marwen.belghith@gmail.com>
2013-02-17 10:57:44 +0100

    devise

--- app/models/credibility.rb
+++ app/models/credibility.rb
@@ -1,6 +1,6 @@
 class Credibility < ActiveRecord::Base
   attr_accessible :articles_id, :confirm, :created_at
-  belongs_to :user 
+  belongs_to :users_id 
 
   validates :users_id, :presence => true
   belongs_to :article 
